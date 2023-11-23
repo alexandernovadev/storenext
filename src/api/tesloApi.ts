38e9054dc74f51db535446f0d283d9d81c5a19e1
alexandernovadev <alexsk88.dev@gmail.com>
2023-11-23 16:51:45 -0500

    Alex - new test configuration

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -2,6 +2,13 @@ import axios from 'axios'
 
 const tesloApi = axios.create({
   baseURL: '/api',
+
+  headers: {
+    // API7:2023 Server Side Request Forgery
+    'Content-Security-Policy': "default-src 'self'; script-src 'none'; object-src 'none';",
+    'X-Content-Type-Options': 'nosniff',
+    'X-Frame-Options': 'DENY'
+  }
 })
 
 export default tesloApi
---
+++ src/test/pages/loginPagen.test.tsx
@@ -0,0 +1,5 @@
+import { render, screen } from '@testing-library/react'
+
+test('Should 1 +1 queal two', () => {
+  expect(1 + 1).toBe(2)
+})
