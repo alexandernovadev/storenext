17727ed8a00f8a84929b50f62a6d8075a5ea6fa9
alexandernovadev <alexsk88.dev@gmail.com>
2023-11-23 23:43:49 -0500

    ALL data is here

--- src/api/tesloApi.ts
+++ src/api/tesloApi.ts
@@ -2,6 +2,15 @@ import axios from 'axios'
 
 const tesloApi = axios.create({
   baseURL: '/api',
+
+  headers: {
+    'Content-Type': 'application/json',
+    // Clickjacking Defense Cheat Sheet
+    "X-Frame-Options": "DENY",
+    // Cross-Site Request Forgery (CSRF) Prevention Cheat Sheet
+    // "X-XSRF-TOKEN": "teslo",
+
+  },
 })
 
 export default tesloApi
